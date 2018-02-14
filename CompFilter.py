99a752873f17e16dbe48b2aede22dc72cfa20f8f
dundunl <duncanlam07@gmail.com>
2018-02-14 06:14:07 +0000

    Completed first implementation and testing of Complimentary Filter

--- CompFilter.py
+++ CompFilter.py
@@ -10,7 +10,7 @@ import L3GD20
 
 class CompFilter():
         
-    def __init__(self, use_gyro):
+    def __init__(self):
         # Create an LSM303 instance.
         self.lsm303 = Adafruit_LSM303.LSM303()
 
@@ -19,27 +19,28 @@ class CompFilter():
         self.f_accel_x = 0.0
         self.f_accel_y = 0.0
         self.f_accel_z = 0.0
-        self.alpha = 0.5
+        self.acc_alpha = 0.1
 
         # Create and configure an L3GD20 instance.
-        if use_gyro:
-            self.l3gd20 = L3GD20.L3GD20(busId = 1, slaveAddr = 0x6b,
-                            ifLog = false, ifWriteBlock = False)
+        self.l3gd20 = L3GD20.L3GD20(busId = 1, slaveAddr = 0x6b,
+                            ifLog = False, ifWriteBlock = False)
 
-            self.use_gyro = True
-            self.l3gd20.Set_PoweR_Mode("Normal")
-            self.l3gd20.Set_FullScale_Value("250dps")
-            self.l3gd20.Set_AxisX_Enabled(True)
-            self.l3gd20.Set_AxisY_Enabled(True)
-            self.l3gd20.Set_AxisZ_Enabled(True)
+        self.use_gyro = True
+        self.l3gd20.Set_PowerMode("Normal")
+        self.l3gd20.Set_FullScale_Value("250dps")
+        self.l3gd20.Set_AxisY_Enabled(True)
 
-            # Initialize and Calibrate gyroscope
-            self.l3gd20.Init()
-            self.l3gd20.Calibrate()
-            self.gyro_dt = 0.02
-            self.gyro_x = 0
-            self.gyro_y = 0
-            self.gyro_z = 0
+        # Initialize and Calibrate gyroscope
+        self.l3gd20.Init()
+##        self.l3gd20.Calibrate()
+        self.gyro_alpha = 0.9
+        self.gyro_dt = 0.02
+        self.gyro_y = 0
+        self.f_gyro_y = 0
+
+        # Initialize Complimentary Filter variables
+        self.comp_alpha = 0.98
+        self.angle = 0
 
     def __call__(self):
         # Read the X, Y, Z axis acceleration values
@@ -49,14 +50,33 @@ class CompFilter():
         accel_x, accel_y, accel_z = accel
                  
         # Run values through a low pass filter
-        self.f_accel_x = accel_x + (self.f_accel_x * (1.0 - self.alpha))
-        self.f_accel_y = accel_y + (self.f_accel_y * (1.0 - self.alpha))
-        self.f_accel_z = accel_z + (self.f_accel_z * (1.0 - self.alpha))
+        self.f_accel_x = self.f_accel_x + self.acc_alpha * (accel_x - self.f_accel_x)
+        self.f_accel_y = self.f_accel_y + self.acc_alpha * (accel_y - self.f_accel_y)
+        self.f_accel_z = self.f_accel_z + self.acc_alpha * (accel_z - self.f_accel_z)
 
-        pitch = (math.atan2(self.f_accel_x, math.sqrt(self.f_accel_y*self.f_accel_y +
+        # Calculate pitch from accelerometer
+        self.pitch = (math.atan2(self.f_accel_x, math.sqrt(self.f_accel_y*self.f_accel_y +
                 self.f_accel_z*self.f_accel_z)) * 180.0/math.pi)
-        print('Pitch = {0}'.format(pitch))
 
-c = CompFilter(False)
+        # Sleep to maintain constant gyro rate of change
+        time.sleep(self.gyro_dt)
+
+        # Read gyro values
+        dy = self.l3gd20.Get_CalOut_Value()
+        old_gyro_y = self.gyro_y
+        self.gyro_y += dy[1]*self.gyro_dt
+
+        # Run values through a high pass filter
+        self.f_gyro_y = (self.gyro_alpha) * (self.f_gyro_y + self.gyro_y - old_gyro_y)
+
+        # Calculate final angle using complimentary filter
+        self.angle = ((1 - self.comp_alpha) * (self.angle + self.f_gyro_y * self.gyro_dt)
+                      + (self.comp_alpha * self.pitch))
+
+        # Print for debugging
+##        print('Pitch = {0}, Gyro = {1} Angle = {2}'.format(self.pitch, self.f_gyro_y, self.angle))
+
+c = CompFilter()
 while True:
     c()
+    print('Angle = {0}'.format(c.angle))
