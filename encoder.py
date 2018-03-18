fb42d0a894590acdd5ec2c9383422288f8ce4d6c
dundunl <duncanlam07@gmail.com>
2018-03-18 20:14:57 +0000

    Code Changes for testing

--- encoder.py
+++ encoder.py
@@ -4,16 +4,13 @@ from time import sleep
 dt = 18
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down= GPIO.PUD_UP)
 
 counter = 0
-clkLastState = GPIO.input(clk)
 
 try:
         while True:
-                dtState = GPIO.input(dt)
-                counter += 1
-                print counter
-                sleep(0.01)
+                print (GPIO.wait_for_edge(dt,GPIO.RISING))
+
 finally:
         GPIO.cleanup()
