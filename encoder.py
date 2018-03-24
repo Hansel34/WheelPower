79e4d58102ecfb7fe5b1c16fa86c25afb4acef6f
Hansel Qiu <Hansel@Hansels-MacBook-Air.local>
2018-03-24 15:47:42 -0600

    changed encoder code

--- encoder.py
+++ encoder.py
@@ -1,7 +1,7 @@
 from RPi import GPIO
 from time import time
 
-ßdt = 23
+dt = 23
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(dt, GPIO.IN, pull_up_down= GPIO.PUD_UP)
@@ -10,11 +10,11 @@ counter = 0
 
 
 while True:
-        t_end = time() + 1
-        counter =0
-        while time() <t_end:
-                GPIO.wait_for_edge(dt,GPIO.RISING)
-                counter += 1
-                print ("There are" +str(counter/10) +"Rotations per second")
+    t_end = time() + 1
+    counter =0
+    while time() <t_end:
+        GPIO.wait_for_edge(dt,GPIO.RISING)
+        counter += 1
+        print ("There are" +str(counter/10) +"Rotations per second")
 
 
