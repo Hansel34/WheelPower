ac75c9e2c7c274111a4aecd14f0531ce96e2d577
Hansel Qiu <Hansel@Hansels-MacBook-Air.local>
2018-03-25 16:55:12 -0600

    fixed indentation

--- main.py
+++ main.py
@@ -20,36 +20,36 @@ debounce = 50
 
 # Signal handler (Ctrl+C)
 def signal_handler(signal, frame):
-    drive.stop()
-    sys.exit()
+	drive.stop()
+	sys.exit()
 
 # Retrieve previous x angles and average them
 def get_angle():
    # angle = numpy.mean( [compFilter() for i in range(debounce)] )
-    tempAngle = 0
-    for x in range(50):
-        compFilter()
-        tempAngle += compFilter.angle
-    angle = tempAngle/50
-    return angle
-    
+	tempAngle = 0
+	for x in range(50):
+		compFilter()
+		tempAngle += compFilter.angle
+	angle = tempAngle/50
+	return angle
+	
 # Move chair based on angle
 def move_chair(angle):
-    print(angle)
-    if abs(angle) > abs(up_thresh):
-        drive.forward()
-        print("Moving forward")
+	print(angle)
+	if abs(angle) > abs(up_thresh):
+		drive.forward()
+		print("Moving forward")
 
-    elif abs(angle) < abs(down_thresh):
-        #drive.backward()
-        print("Moving backwards")
+	elif abs(angle) < abs(down_thresh):
+		#drive.backward()
+		print("Moving backwards")
 
-    else:
-        drive.stop()
-        print("Not assisting");
+	else:
+		drive.stop()
+		print("Not assisting");
 
 # Main
 signal.signal(signal.SIGINT, signal_handler)
 while True:
-    angle = get_angle()
-    move_chair(angle)
+	angle = get_angle()
+	move_chair(angle)
