819aa1743732ad14a11a77dffe060a13a3eb5c6d
Hansel Qiu <qiu_hansel@hotmail.com>
2018-03-26 16:20:28 -0600

    added base angle initialization

--- main.py
+++ main.py
@@ -3,44 +3,33 @@ import signal
 import sys
 import numpy
 
-<<<<<<< HEAD
 from compFilterv2 import *
-=======
-from CompFilter import *
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
 from driverClass import *
 
 # Initialize driver/sensor classes
 drive = driver()
 compFilter = CompFilter()
-
-<<<<<<< HEAD
-# Set up variables
-flat_thresh = input("PLEASE ENTER ANGLE: ")
-down_thresh = flat_thresh - 2
-up_thresh = flat_thresh + 2
-debounce = 50
 ignoreAngles = 500
-=======
-
-
-# Set up variables
-flat_thresh = input("pLEASE ENTER ANGLE: ")
-down_thresh = flat_thresh + 2
-up_thresh = flat_thresh - 2
-debounce = 50
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
 
 # Signal handler (Ctrl+C)
 def signal_handler(signal, frame):
 	drive.stop()
 	sys.exit()
-<<<<<<< HEAD
+
 # ignore first 10 angles
 def ignore_angle():
         print("Please wait for initialization")
         for x in range(ignoreAngles):
                 compFilter()
+# setup base angles and values
+def get_base_angle():
+	tempAngle = 0
+    for x in range(20):
+        compFilter()
+        tempAngle += compFilter.angle
+    flat_thresh = tempAngle/20
+    down_thresh = flat_thresh - 2
+	up_thresh = flat_thresh + 2
 # Retrieve previous x angles and average them
 def get_angle():
    # angle = numpy.mean( [compFilter() for i in range(debounce)] )
@@ -69,36 +58,7 @@ def move_chair(angle):
 # Main
 signal.signal(signal.SIGINT, signal_handler)
 ignore_angle()
-=======
-
-# Retrieve previous x angles and average them
-def get_angle():
-   # angle = numpy.mean( [compFilter() for i in range(debounce)] )
-	tempAngle = 0
-	for x in range(50):
-		compFilter()
-		tempAngle += compFilter.angle
-	angle = tempAngle/50
-	return angle
-	
-# Move chair based on angle
-def move_chair(angle):
-	print(angle)
-	if abs(angle) > abs(up_thresh):
-		drive.forward()
-		print("Moving forward")
-
-	elif abs(angle) < abs(down_thresh):
-		#drive.backward()
-		print("Moving backwards")
-
-	else:
-		drive.stop()
-		print("Not assisting");
-
-# Main
-signal.signal(signal.SIGINT, signal_handler)
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
+get_base_angle()
 while True:
 	angle = get_angle()
 	move_chair(angle)
