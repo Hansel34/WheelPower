6e773515d67c0efb5a13f5b14b0d5b70a2a25152
Hansel Qiu <qiu_hansel@hotmail.com>
2018-03-26 16:30:04 -0600

    fixed commit issues

--- compFilterv2.py
+++ compFilterv2.py
@@ -14,16 +14,6 @@ class CompFilter():
         # Create an LSM303 instance.
         self.lsm303 = Adafruit_LSM303.LSM303()
 
-        # Initialize values to hold low pass filter
-        # values for accelerometer
-        self.f_accel_x = 0.0
-        self.f_accel_y = 0.0
-        self.f_accel_z = 0.0
-<<<<<<< HEAD
-=======
-        self.acc_alpha = 0.02
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
-
         # Create and configure an L3GD20 instance.
         self.l3gd20 = L3GD20.L3GD20(busId = 1, slaveAddr = 0x6b,
                             ifLog = False, ifWriteBlock = False)
@@ -35,14 +25,8 @@ class CompFilter():
 
         # Initialize and Calibrate gyroscope
         self.l3gd20.Init()
-<<<<<<< HEAD
+
         self.gyro_dt = 0.004
-=======
-        self.gyro_alpha = 0.9
-        self.gyro_dt = 0.02
-        self.gyro_y = 0
-        self.f_gyro_y = 0
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
 
         # Initialize Complimentary Filter variables
         self.comp_alpha = 0.98
@@ -54,44 +38,20 @@ class CompFilter():
                     
         # Grab the X, Y, Z components from the reading and print them out.
         accel_x, accel_y, accel_z = accel
-<<<<<<< HEAD
 
         # Calculate pitch from accelerometer
         self.pitch = (math.atan2(accel_x, math.sqrt(accel_y*accel_y +
                 accel_z*accel_z)) * 180.0/math.pi)
-=======
-                 
-        # Run values through a low pass filter
-        #self.f_accel_x = self.f_accel_x + self.acc_alpha * (accel_x - self.f_accel_x)
-        #self.f_accel_y = self.f_accel_y + self.acc_alpha * (accel_y - self.f_accel_y)
-        #self.f_accel_z = self.f_accel_z + self.acc_alpha * (accel_z - self.f_accel_z)
-
-        # Calculate pitch from accelerometer
-        self.pitch = (math.atan2(self.accel_x, math.sqrt(self.accel_y*self.accel_y +
-                self.accel_z*self.accel_z)) * 180.0/math.pi)
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
 
         # Sleep to maintain constant gyro rate of change
         time.sleep(self.gyro_dt)
 
         # Read gyro values
         dy = self.l3gd20.Get_CalOut_Value()
-<<<<<<< HEAD
-=======
-        #old_gyro_y = self.gyro_y
-        #self.gyro_y += dy[1]*self.gyro_dt
-
-        # Run values through a high pass filter
-        #self.f_gyro_y = (self.gyro_alpha) * (self.f_gyro_y + self.gyro_y - old_gyro_y)
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
 
         # Calculate final angle using complimentary filter
         self.angle = (self.comp_alpha * (self.angle + dy[1] * self.gyro_dt)
                       + ((1-self.comp_alpha) * self.pitch))
 
         # Print for debugging
-<<<<<<< HEAD
         #print('Pitch = {0}, Gyro = {1} Angle = {2}'.format(self.pitch, dy[1] * self.gyro_dt, self.angle))
-=======
-        print('Pitch = {0}, Gyro = {1} Angle = {2}'.format(self.pitch, self.f_gyro_y, self.angle))
->>>>>>> 7b698ebdd7ab8dbc6bfbdc7d315868df092ccd26
--- main.py
+++ main.py
@@ -18,41 +18,41 @@ def signal_handler(signal, frame):
 
 # ignore first 10 angles
 def ignore_angle():
-        print("Please wait for initialization")
-        for x in range(ignoreAngles):
-                compFilter()
+	print("Please wait for initialization")
+	for x in range(ignoreAngles):
+		compFilter()
 # setup base angles and values
 def get_base_angle():
 	tempAngle = 0
-        for x in range(20):
-                compFilter()
-                tempAngle += compFilter.angle
-        flat_thresh = tempAngle/20
-        down_thresh = flat_thresh - 2
-        up_thresh = flat_thresh + 2
+	for x in range(20):
+		compFilter()
+		tempAngle += compFilter.angle
+	flat_thresh = tempAngle/20
+	down_thresh = flat_thresh - 2
+	up_thresh = flat_thresh + 2
 # Retrieve previous x angles and average them
 def get_angle():
    # angle = numpy.mean( [compFilter() for i in range(debounce)] )
-    tempAngle = 0
-    for x in range(20):
-        compFilter()
-        tempAngle += compFilter.angle
-    angle = tempAngle/20
-    return angle
-    
+	tempAngle = 0
+	for x in range(20):
+		compFilter()
+		tempAngle += compFilter.angle
+	angle = tempAngle/20
+	return angle
+	
 # Move chair based on angle
 def move_chair(angle):
-    print(angle)
-    if angle > up_thresh:
-        drive.forward()
+	print(angle)
+	if angle > up_thresh:
+		drive.forward()
 
 
-    elif angle < down_thresh:
-        #drive.backward()
+	#elif angle < down_thresh:
+		#drive.backward()
 
 
-    else:
-        drive.stop()
+	else:
+		drive.stop()
  
 
 # Main
