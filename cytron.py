79cda8fa952531698b30df62343c662820344888
hansel34 <qiu_hansel@hotmail.com>
2018-03-31 15:46:56 -0600

    switched motors

--- cytron.py
+++ cytron.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO			# using Rpi.GPIO module
 from time import sleep			# import function sleep for delay
 GPIO.setmode(GPIO.BCM)			# GPIO numbering
 GPIO.setwarnings(False)			# enable warning from GPIO
-AN1 = 12				# set pwm1 pin on MD10-hat
-DIG1 = 26				# set dir1 pin on MD10-Hat
-GPIO.setup(AN1, GPIO.OUT)		# set pin as output
-GPIO.setup(DIG1, GPIO.OUT)		# set pin as output
+AN2 = 13				# set pwm2 pin on MD10-Hat
+DIG2 = 24				# set dir2 pin on MD10-Hat
+GPIO.setup(AN2, GPIO.OUT)		# set pin as output
+GPIO.setup(DIG2, GPIO.OUT)		# set pin as output
 sleep(1)				# delay for 1 seconds
-p1 = GPIO.PWM(DIG1, 100)		# set pwm for M1
+p2 = GPIO.PWM(DIG2, 100)		# set pwm for M2
 
 speed = int(input())
 
@@ -17,28 +17,28 @@ if (speed > 50) :
     while True:
 
      print "STOP"			# display "Forward" when programe run
-     GPIO.output(AN1, GPIO.LOW)		# set AN1 as HIGH, M1B will turn ON
-     p1.start(0)				# set Direction for M1
+     GPIO.output(AN2, GPIO.LOW)		# set AN2 as HIGH, M2B will turn ON
+     p2.start(0)				# set Direction for M2  
      sleep(1)				#delay for 2 second
                            
 
      print "GO"
-     GPIO.output(AN1, GPIO.HIGH)           # set AN1 as LOW, M1B will STOP
-     p1.start(speed)                                             
+     GPIO.output(AN2, GPIO.HIGH)           # set AN2 as HIGH, M2B will STOP                       
+     p2.start(speed)                         
      sleep(1)                             #delay for 3 second
 
-     GPIO.output(AN1, GPIO.HIGH)           # set AN1 as LOW, M1B will STOP
-     p1.start(speed)                                                 
+     GPIO.output(AN2, GPIO.HIGH)           # set AN2 as HIGH, M2B will STOP                        
+     p2.start(speed)                         
      sleep(1)
 
      print "at full speed"
-     GPIO.output(AN1, GPIO.HIGH)           # set AN1 as LOW, M1B will STOP
-     p1.start(speed)                                                  
+     GPIO.output(AN2, GPIO.HIGH)           # set AN2 as HIGH, M2B will STOP                         
+     p2.start(speed)                         
      sleep(100)    
 
   except:					# exit programe when keyboard interupt
-     p1.start(50)				# set speed to 0
-     GPIO.output(AN1, GPIO.LOW)           # set AN1 as LOW, M1B will STOP
+     p2.start(50)				# set speed to 0
+     GPIO.output(AN2, GPIO.LOW)  
                                           # Control+X to save and exit
 else:
 
@@ -46,25 +46,25 @@ else:
     while True:
 
      print "STOP"			# display "Forward" when programe run
-     GPIO.output(AN1, GPIO.LOW)		# set AN1 as HIGH, M1B will turn ON
-     p1.start(0)				# set Direction for M1 
+     GPIO.output(AN2, GPIO.LOW)		# set AN2 as HIGH, M2B will turn ON
+     p2.start(0)				# set Direction for M2  
      sleep(1)				#delay for 2 second
                            
 
      print "GO"
-     GPIO.output(AN1, GPIO.HIGH)           # set AN1 as LOW, M1B will STOP
-     p1.start(speed)                                                 
+     GPIO.output(AN2, GPIO.HIGH)           # set AN2 as HIGH, M2B will STOP                          
+     p2.start(speed)                         
      sleep(1)                             #delay for 3 second
 
-     GPIO.output(AN1, GPIO.HIGH)           # set AN1 as LOW, M1B will STOP
-     p1.start(speed)                                                 
+     GPIO.output(AN2, GPIO.HIGH)           # set AN2 as HIGH, M2B will STOP                       
+     p2.start(speed)                         
      sleep(1)
 
-     GPIO.output(AN1, GPIO.HIGH)           # set AN1 as LOW, M1B will STOP
-     p1.start(speed)                                               
+     GPIO.output(AN2, GPIO.HIGH)           # set AN2 as HIGH, M2B will STOP                         
+     p2.start(speed)                         
      sleep(100)    
 
   except:					# exit programe when keyboard interupt
-     p1.start(50)				# set speed to 0
-     GPIO.output(AN1, GPIO.LOW)           # set AN1 as LOW, M1B will STOP
+     p2.start(50)				# set speed to 0
+     GPIO.output(AN2, GPIO.LOW)  
                                           # Control+X to save and exit
